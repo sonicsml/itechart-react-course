4e2bacdff446bdabccaad2d257b38cd0559e2a1f
Polovkov Dmitry <sonics.scs@gmail.com>
2021-09-21 00:31:12 +0300

    fix: fixed errors in the code

--- src/App.js
+++ src/App.js
@@ -3,10 +3,27 @@ import Header from './components/Header';
 import './App.css';
 
 function App() {
+  const infocards = [
+    {
+        id: 1,
+        title: 'Card 1',
+        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+    },
+    {
+      id: 2,
+      title: 'Card 2',
+      description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+  },
+  {
+      id: 3,
+      title: 'Card 1',
+      description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+  }
+]
   return (
     <div>
-    <Header sitename="itechart-react-course - React-components"></Header>,
-    <AllCards></AllCards>
+    <Header sitename="itechart-react-course - React-components" />,
+    <AllCards itemts={infocards}></AllCards>
     </div>
   );
 }
--- src/components/AllCards.js
+++ src/components/AllCards.js
@@ -1,37 +1,20 @@
 import './AllCards.css';
 import Card from './Card';
 
-function AllCards() {
-  const infocards = [
-      {
-          id: 1,
-          title: 'Card 1',
-          description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
-      },
-      {
-        id: 2,
-        title: 'Card 2',
-        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
-    },
-    {
-        id: 3,
-        title: 'Card 1',
-        description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
-    }
-  ]
+function AllCards(props) {
 
   return (
         <div className="flex container-cards">
             <Card 
-                title={infocards[0].title}
+                title={props.infocards[0].title}
                 description={infocards[0].description}
             ></Card>
             <Card 
-                title={infocards[1].title}
+                title={props.infocards[1].title}
                 description={infocards[1].description}
             ></Card>
             <Card 
-                title={infocards[2].title}
+                title={props.infocards[2].title}
                 description={infocards[2].description}
             ></Card>
         </div>
