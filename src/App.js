53cdf73525d8e53aad9ce36179a9136f332e1f53
Aliaksei Vysotsky <alexeyvysotsky97@gmail.com>
2021-09-23 23:10:49 +0300

    feat: Fixing validation

--- src/App.js
+++ src/App.js
@@ -1,42 +1,41 @@
-import React, { useState } from 'react';
-import AllCards from './components/Cards/AllCards';
-import Header from './components/UI/Header';
-import AddCard from './components/Cards/AddCard';
-import './App.css';
+import React, { useState } from "react";
+import AllCards from "./components/Cards/AllCards";
+import Header from "./components/UI/Header";
+import AddCard from "./components/Cards/AddCard";
+import "./App.css";
 
 const CARD_DATA = [
-  {   
-    id: '1',
-    title: 'Card 1',
-    description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
-  },
-  { 
-    id: '2',
-    title: 'Card 2',
-    description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
+  {
+    id: "1",
+    title: "Card 1",
+    description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
   },
   {
-    id: '3',
-    title: 'Card 1',
-    description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum'
-  }
-]
+    id: "2",
+    title: "Card 2",
+    description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+  },
+  {
+    id: "3",
+    title: "Card 1",
+    description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+  },
+];
 const App = () => {
   const [cards, setCards] = useState(CARD_DATA);
   const addCardHandler = (addFromData) => {
-    setCards((prevCards) => { 
+    setCards((prevCards) => {
       return [addFromData, ...prevCards];
     });
-  }
+  };
   return (
-    <div className="container"> 
-    <Header sitename="itechart-react-course - React-components"></Header>
-    <AllCards items={cards} />
-    
-    <AddCard onAddCard={addCardHandler} />
-  
+    <div className="container">
+      <Header sitename="itechart-react-course - React-components" />
+      <AllCards items={cards} />
+
+      <AddCard onAddCard={addCardHandler} />
     </div>
   );
-}
+};
 
 export default App;
